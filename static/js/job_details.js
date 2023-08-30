f9066f6c1be27a8a0f3de272ed1820a921b17e75
Arun mohan <arunmohan0095@gmail.com>
2023-08-30 10:29:19 +0530

    Job details page added

--- static/js/job_details.js
+++ static/js/job_details.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 
+    var applyButton = document.getElementById("apply-button");
+    var applyUrl = applyButton.getAttribute("data-job-url"); 
+
+    applyButton.addEventListener("click", function() {
+        window.location.href = applyUrl;
+    });
 });
